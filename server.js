8585f6a3a64b658bb079397553c56e61dc2f1a1a
Satilog-Suporte <suporte@satilog.com.br>
2025-07-04 16:31:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // --- Configurações do Servidor ---
 app.use(bodyParser.urlencoded({ extended: true }));
